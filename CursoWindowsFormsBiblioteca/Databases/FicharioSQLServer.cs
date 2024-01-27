3e788b3480de44118e2569484682248d734f7dd1
Matheus Luchsinger <102822195+luquisinger@users.noreply.github.com>
2024-01-27 13:22:39 -0300

    sqlserver pt1

---
+++ CursoWindowsFormsBiblioteca/Databases/FicharioSQLServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class FicharioSQLServer
+    {
+        public string mensagem;
+        public bool status;
+        public string tabela;
+        public SQLServerClass db;
+
+
+
+
+    }
+}
---
+++ CursoWindowsFormsBiblioteca/Databases/SQLServerClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class SQLServerClass
+    {
+        public string stringConn;
+        public SqlConnection connDB;
+
+        public SQLServerClass()
+        {
+            try
+            {
+                stringConn = "DData Source=LUQUISINGER;Initial Catalog=ByteBank;Persist Security Info=True;User ID=sa;Password=12";
+                connDB = new SqlConnection(stringConn);
+                connDB.Open();
+            } 
+            catch (Exception ex) 
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
